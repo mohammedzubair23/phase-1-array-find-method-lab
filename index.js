067df154850f3a4d4c02cc640148e47fd2037aa7
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-13 09:57:45 -0500

    commit tree d3d13516b8c5e71755c037a94d4101c70507f834

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+ function superbowlWin(records) {
+    const superBowlWins = records.find(record => record.result === "W")
+        if(superBowlWins) {
+            return superBowlWins.year
+        } else {
+            return undefined 
+        }
+ }
+  
